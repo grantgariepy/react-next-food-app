b613e5f656180d71a8ba921ee37ab7a117af05af
grantgariepy <grantgariepy@protonmail.com>
2022-01-17 10:00:43 -0700

    layout

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
+  
       <Head>
         <title>Pizza Time Pizza in Utah</title>
         <meta name="description" content="Best pizza shop in town!" />
