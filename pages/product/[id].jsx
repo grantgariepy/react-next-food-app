d2592e558a301fa569a8ecd5309d60f7522c59e9
grantgariepy <grantgariepy@protonmail.com>
2022-01-18 20:26:47 -0700

    products page almost done

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,12 +1,14 @@
 import styles from "../../styles/Prodcut.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 const Product = () => {
+    const [size, setSize] = useState(0)
     const pizza = {
         id: 1,
         img: "/img/pizza.png",
         name: "CAMPAGNOLA",
-        price: [19.9, 23.9, 27.9],
+        price: [19.99, 23.99, 27.99],
         desc: "Lorem ipsum dolor sit amet, consectetur adipsicing elit."
     };
 
@@ -19,9 +21,26 @@ const Product = () => {
             </div>
             <div className={styles.right}>
                 <h1 className={styles.title}>{pizza.name}</h1>
+                <span className={styles.price}>${pizza.price[size]}</span>
+                <p className={styles.desc}>{pizza.desc}</p>
+                <h3 className={styles.choose}>Choose the size</h3>
+                <div className={styles.sizes}>
+                    <div className={styles.size} onClick={()=>setSize(0)}>
+                        <Image alt="" src="/img/size.png" layout="fill"/>
+                        <span className={styles.number}>Small</span>
+                    </div>
+                    <div className={styles.size} onClick={()=>setSize(1)}> 
+                        <Image alt="" src="/img/size.png" layout="fill"/>
+                        <span className={styles.number}>Medium</span>
+                    </div>
+                    <div className={styles.size} onClick={()=>setSize(2)}>
+                        <Image alt="" src="/img/size.png" layout="fill"/>
+                        <span className={styles.number}>Large</span>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Product
