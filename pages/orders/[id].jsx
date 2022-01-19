5c6f7c4e970c2cf586b354520eb886e43539bb7a
grantgariepy <grantgariepy@protonmail.com>
2022-01-18 21:20:15 -0700

    df

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,14 @@
+import styles from "../../styles/Order.module.css";
+import Image from "next/image";
+
+
+const Order = () => {
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );    
+};
+
+export default Order;
