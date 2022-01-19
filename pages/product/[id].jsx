623004128c93cb63c5fc40378d352337d2bf840b
grantgariepy <grantgariepy@protonmail.com>
2022-01-18 20:38:05 -0700

    prodcut page mostly done

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -38,6 +38,29 @@ const Product = () => {
                         <span className={styles.number}>Large</span>
                     </div>
                 </div>
+                <h3 className={styles.choose}>Choose additional ingredients</h3>
+                <div className={styles.ingredients}>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="double" name="double" className={styles.checkbox} />
+                        <label htmlFor="double">Double Ingredients</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="cheese" name="cheese" className={styles.checkbox} />
+                        <label htmlFor="cheese">Extre Cheese</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input type="checkbox" id="spicy" name="spicy" className={styles.checkbox} />
+                        <label htmlFor="spicy">Spicy Sauce</label>
+                    </div>    
+                    <div className={styles.option}>
+                        <input type="checkbox" id="garlic" name="garlic" className={styles.checkbox} />
+                        <label htmlFor="garlic">Garlic Sauce</label>
+                    </div>
+                </div>
+                <div className={styles.add}>
+                    <input type="number" defaultValue={1} className={styles.quantity} />
+                    <button className={styles.button}>Add to cart</button>
+                </div>
             </div>
         </div>
     );
