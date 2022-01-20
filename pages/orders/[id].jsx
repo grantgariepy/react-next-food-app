dad7ec676af2895863dbb940894bb5246c3a4cae
grantgariepy <grantgariepy@protonmail.com>
2022-01-19 20:19:40 -0700

    order page done and cart started

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -4,13 +4,92 @@ import Image from "next/image";
 
 const Order = () => {
 
+    const status = 0;
+
+    const statusClass = (index)=>{
+        if(index-status < 1) return styles.done
+        if(index-status === 1) return styles.inProgress
+        if(index-status > 1) return styles.undone
+    }
+
     return (
         <div className={styles.container}>
-        <div className={left}></div>
-        <div className={right}></div>
+            <div className={styles.left}>
+                <div className={styles.row}>
+                    <table className={styles.table}>
+                        <tr className={styles.tr}>
+                            <th>Order ID</th>
+                            <th>Customer</th>
+                            <th>Address</th>
+                            <th>Total</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <span className={styles.id}>1238731988</span>
+                            </td>
+                            <td>
+                                <span className={styles.name}>
+                                    John Doe
+                                </span>
+                            </td>
+                            <td>
+                                <span className={styles.address}>123 Main St SLC, UT </span>
+                            </td>
+                            <td>
+                                <span className={styles.total}>$79.80</span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div className={styles.row}>
+                    <div className={statusClass(0)}>
+                        <Image alt="" src="/img/paid.png" width={30} height={30} />
+                        <span>Payment</span>
+                        <div className={styles.checkedIcon}>
+                            <Image alt="" src="/img/checked.png" width={20} height={20} />
+                        </div>
+                    </div>
+                    <div className={statusClass(1)}>
+                        <Image alt="" src="/img/bake.png" width={30} height={30} />
+                        <span>Preparing</span>
+                        <div className={styles.checkedIcon}>
+                            <Image alt="" src="/img/checked.png" width={20} height={20} />
+                        </div>
+                    </div>
+                    <div className={statusClass(2)}>
+                        <Image alt="" src="/img/bike.png" width={30} height={30} />
+                        <span>On the way</span>
+                        <div className={styles.checkedIcon}>
+                            <Image alt="" src="/img/checked.png" width={20} height={20} />
+                        </div>
+                    </div>
+                    <div className={statusClass(3)}>
+                        <Image alt="" src="/img/delivered.png" width={30} height={30} />
+                        <span>Delivered</span>
+                        <div className={styles.checkedIcon}>
+                            <Image alt="" src="/img/checked.png" width={20} height={20} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.right}>
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>CART TOTAL</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total:</b>$79.60
+                    </div>
+                    <button disabled className={styles.button}>PAID</button>
+                </div>
+            </div>
 
         </div>
-    );    
+    );
 };
 
 export default Order;
