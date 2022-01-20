66d8243b6acf283707bca3d90ddeae8c2657d0f3
grantgariepy <grantgariepy@protonmail.com>
2022-01-19 20:53:48 -0700

    mobile cart start

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,7 +6,7 @@ const Cart = () => {
         <div className={styles.container}>
             <div className={styles.left}>
                 <table className={styles.table}>
-                    <tr className={styles.tr}>
+                    <tr className={styles.trTitle}>
                         <th>Product</th>
                         <th>Name</th>
                         <th>Extras</th>
@@ -14,7 +14,7 @@ const Cart = () => {
                         <th>Quantity</th>
                         <th>Total</th>
                     </tr>
-                    <tr>
+                    <tr className={styles.tr}>
                         <td>
                             <div className={styles.imgContainer}>
                                 <Image src="/img/pizza.png" layout="fill" objectFit="cover" alt="" />
@@ -38,7 +38,7 @@ const Cart = () => {
                             <span className={styles.total}>$39.80</span>
                         </td>
                     </tr>
-                    <tr>
+                    <tr className={styles.tr}> 
                         <td>
                             <div className={styles.imgContainer}>
                                 <Image src="/img/pizza.png" layout="fill" objectFit="cover" alt="" />
