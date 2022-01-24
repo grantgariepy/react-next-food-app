16f4795bbb1fac3a1a6c4e7d10b8a8fd05665895
grantgariepy <grantgariepy@protonmail.com>
2022-01-23 20:04:56 -0700

    updated logo

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,10 @@ export default function Home() {
         <title>Pizza Time Pizza in Utah!</title>
         <meta name="description" content="Best pizza shop in town!" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet"></link>
+        
       </Head>
 
       <Featured/>
