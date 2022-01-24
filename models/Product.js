5c35d9d3b4852f005d400052732f05e6a22bfb3a
grantgariepy <grantgariepy@protonmail.com>
2022-01-23 21:48:50 -0700

    mongo db created pizza collection FINALLY

--- models/Product.js
+++ models/Product.js
@@ -1,36 +1,36 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        maxlength: 60,
+      type: String,
+      required: true,
+      maxlength: 60,
     },
     desc: {
-        type: String,
-        required: true,
-        maxlength: 200,
+      type: String,
+      required: true,
+      maxlength: 200,
     },
     img: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     prices: {
-        type: [Number],
-        required: true,
+      type: [Number],
+      required: true,
     },
     extraOptions: {
-        type: [
-            {
-                text: { type: String, required: true },
-                price: { type: Number, required: true },
-            },
-        ],
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
     },
   },
-    { timestamps: true }
+  { timestamps: true }
 );
 
-
-export default mongoose.models.Product || 
-    mongoose.model("Product", ProductSchema);
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
