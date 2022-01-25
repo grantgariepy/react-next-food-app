39ca4e18b980b34f4ce17b8e0d3a66b124bdd7aa
grantgariepy <grantgariepy@protonmail.com>
2022-01-24 20:50:35 -0700

    extras added

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,16 +4,32 @@ import { useState } from "react";
 import axios from "axios";
 
 const Product = ({pizza}) => {
+    const [price, setPrice] = useState(pizza.prices[0]);
     const [size, setSize] = useState(0);
+    const [extras, setExtras] = useState([]);
+
+    const changePrice = (number) =>{
+        setPrice(price+number);
+    }
+    const handleSize = (sizeIndex)=>{
+        const difference = pizza.prices[sizeIndex] - pizza.prices[size];
+        setSize(sizeIndex);
+        changePrice(difference);
+    }
     
 
     const handleChange = (e, option) => {
         const checked = e.target.checked;
 
         if(checked){
-            
+            changePrice(option.price);
+            setExtras(prev=>[...prev,option]);
+        }else{
+            changePrice(-option.price);
+            setExtras(extras.filter(extra=>extra._id !== option._id));
         }
     };
+
     return (
         <div className={styles.container}>
             <div className={styles.left}>
@@ -23,19 +39,19 @@ const Product = ({pizza}) => {
             </div>
             <div className={styles.right}>
                 <h1 className={styles.title}>{pizza.title}</h1>
-                <span className={styles.price}>${pizza.prices[size]}</span>
+                <span className={styles.price}>${price}</span>
                 <p className={styles.desc}>{pizza.desc}</p>
                 <h3 className={styles.choose}>Choose the size</h3>
                 <div className={styles.sizes}>
-                    <div className={styles.size} onClick={() => setSize(0)}>
+                    <div className={styles.size} onClick={() => handleSize(0)}>
                         <Image alt="" src="/img/size.png" layout="fill" />
                         <span className={styles.number}>Small</span>
                     </div>
-                    <div className={styles.size} onClick={() => setSize(1)}>
+                    <div className={styles.size} onClick={() => handleSize(1)}>
                         <Image alt="" src="/img/size.png" layout="fill" />
                         <span className={styles.number}>Medium</span>
                     </div>
-                    <div className={styles.size} onClick={() => setSize(2)}>
+                    <div className={styles.size} onClick={() => handleSize(2)}>
                         <Image alt="" src="/img/size.png" layout="fill" />
                         <span className={styles.number}>Large</span>
                     </div>
