6f3194cb66e0bf0b9526cd3fc081ad1b442affae
grantgariepy <grantgariepy@protonmail.com>
2022-01-25 21:17:45 -0700

    order page mostly done

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,10 +1,11 @@
 import styles from "../../styles/Order.module.css";
 import Image from "next/image";
+import axios from "axios";
 
 
-const Order = () => {
+const Order = ({order}) => {
 
-    const status = 0;
+    const status = order.status;
 
     const statusClass = (index) => {
         if (index - status < 1) return styles.done
@@ -26,18 +27,18 @@ const Order = () => {
                             </tr>
                             <tr className={styles.tr}>
                                 <td>
-                                    <span className={styles.id}>1238731988</span>
+                                    <span className={styles.id}>{order._id}</span>
                                 </td>
                                 <td>
                                     <span className={styles.name}>
-                                        John Doe
+                                        {order.customer}
                                     </span>
                                 </td>
                                 <td>
-                                    <span className={styles.address}>123 Main St SLC, UT </span>
+                                    <span className={styles.address}>{order.address}</span>
                                 </td>
                                 <td>
-                                    <span className={styles.total}>$79.80</span>
+                                    <span className={styles.total}>${order.total}</span>
                                 </td>
                             </tr>
                         </tbody>
@@ -78,13 +79,13 @@ const Order = () => {
                 <div className={styles.wrapper}>
                     <h2 className={styles.title}>CART TOTAL</h2>
                     <div className={styles.totalText}>
-                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+                        <b className={styles.totalTextTitle}>Subtotal:</b>${order.total}
                     </div>
                     <div className={styles.totalText}>
                         <b className={styles.totalTextTitle}>Discount:</b>$0.00
                     </div>
                     <div className={styles.totalText}>
-                        <b className={styles.totalTextTitle}>Total:</b>$79.60
+                        <b className={styles.totalTextTitle}>Total:</b>${order.total}
                     </div>
                     <button disabled className={styles.button}>PAID</button>
                 </div>
@@ -92,6 +93,13 @@ const Order = () => {
 
         </div>
     );
+};
+
+export const getServerSideProps = async ({ params }) => {
+    const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+    return {
+        props: { order: res.data },
+    };
 };
 
 export default Order;
